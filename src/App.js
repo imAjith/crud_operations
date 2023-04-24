0480de887b66a3e40147339f5e6041b730f22a6a
Ajith <imAjith@protonmail.ch>
2023-04-25 00:29:55 +0530

    crud-op

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Modal } from './components/Model/Modal';
+import { Table } from './components/Table';
 
 function App() {
+  const [modelOpen, setModelOpen] = useState(false);
+  const [rows, setRows] =useState([
+    {
+      page:"Page 1",description:"This is the first page",status:'Live'
+    },
+    {
+      page:"Page 2",description:"This is the second page",status:'Draft'
+    },
+    {
+      page:"Page 3",description:"This is the third page",status:'Error'
+    }
+  ]);
+
+  const handleDelete = (value)=>{
+    setRows(rows.filter((_,idx) => idx !== value))
+  }
+
+  const handleSubmitFun = (value)=>{
+    setRows([...rows,value])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <Table rows={rows} deleteRow={handleDelete} />
+    <button className='btn' onClick={()=>{setModelOpen(true)}}>Add</button>
+    {modelOpen &&<Modal closeModel={()=>{setModelOpen(false)}} onSubmit={handleSubmitFun}/>}
     </div>
   );
 }
